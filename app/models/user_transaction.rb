ff55c0db09b21300ed9a7337cf9d8556853c4444
S1azZy <a.tselovalnikov@netology.ru>
2022-12-01 17:00:17 +0800

    add config

--- app/models/user_transaction.rb
+++ app/models/user_transaction.rb
@@ -1,5 +1,20 @@
 # frozen_string_literal: true
 
 class UserTransaction < ApplicationRecord
-  belongs_to :user
+  belongs_to :user_wallet
+  belongs_to :currency
+  belongs_to :initiator_document, polymorphic: true
+
+  validates :income,
+            :expense,
+            allow_blank: true,
+            numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100_000_000 }
+  validate :income_or_expense_must_be_set
+
+
+  def income_or_expense_must_be_set
+    if expense.zero? && income.zero?
+      errors.add(:income, "Должен быть установлен приход или расход!")
+    end
+  end
 end
